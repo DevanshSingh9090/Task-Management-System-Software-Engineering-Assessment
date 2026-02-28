2f4b514c4c464b2ae2c94214f365b3a606bf7f5c
Devansh Singh <devanshsingh9090@gmail.com>
2026-02-28 16:38:50 +0530

    Fix JWT file

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -8,21 +8,35 @@ export interface AppJwtPayload {
 }
 
 export const signAccessToken = (payload: AppJwtPayload): string => {
-  return jwt.sign(payload, config.JWT_ACCESS_SECRET as string, {
-    expiresIn: config.ACCESS_TOKEN_EXPIRES_IN
-  });
+  return jwt.sign(
+    payload,
+    config.JWT_ACCESS_SECRET as string,
+    {
+      expiresIn: config.ACCESS_TOKEN_EXPIRES_IN as jwt.SignOptions["expiresIn"]
+    }
+  );
 };
 
 export const signRefreshToken = (payload: AppJwtPayload): string => {
-  return jwt.sign(payload, config.JWT_REFRESH_SECRET as string, {
-    expiresIn: config.REFRESH_TOKEN_EXPIRES_IN
-  });
+  return jwt.sign(
+    payload,
+    config.JWT_REFRESH_SECRET as string,
+    {
+      expiresIn: config.REFRESH_TOKEN_EXPIRES_IN as jwt.SignOptions["expiresIn"]
+    }
+  );
 };
 
 export const verifyAccessToken = (token: string): AppJwtPayload => {
-  return jwt.verify(token, config.JWT_ACCESS_SECRET as string) as AppJwtPayload;
+  return jwt.verify(
+    token,
+    config.JWT_ACCESS_SECRET as string
+  ) as AppJwtPayload;
 };
 
 export const verifyRefreshToken = (token: string): AppJwtPayload => {
-  return jwt.verify(token, config.JWT_REFRESH_SECRET as string) as AppJwtPayload;
+  return jwt.verify(
+    token,
+    config.JWT_REFRESH_SECRET as string
+  ) as AppJwtPayload;
 };
